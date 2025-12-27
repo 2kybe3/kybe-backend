f29f1955d319d4128b8de9026d538119e04fb9b1
2kybe3 <kybe@kybe.xyz>
2025-12-27 18:19:52 +0100

    switch to serenitys discord webhook integration (use "" to not need a token unauthenticated)

--- src/notifications/providers/discord.rs
+++ src/notifications/providers/discord.rs
@@ -1,49 +1,38 @@
 use crate::notifications::error::NotificationError;
 use crate::notifications::{Notification, Notifier};
-use reqwest::{Client, StatusCode};
-use webhook::models::Message;
+use poise::serenity_prelude::{CreateEmbed, ExecuteWebhook, Http, Webhook};
 
 #[derive(Debug)]
 pub struct DiscordNotifier {
     url: String,
-    client: Client,
+    http: Http,
 }
 
 impl DiscordNotifier {
     pub fn new<S: Into<String>>(url: S) -> Self {
-        Self { url: url.into(), client: Client::new() }
+        let http = Http::new("");
+        Self { url: url.into(), http }
     }
 }
 
-// TODO: use serenitys webhook system later to remove webhook dep
 #[async_trait::async_trait]
 impl Notifier for DiscordNotifier {
     async fn send(&self, notification: &Notification) -> Result<(), NotificationError> {
-        let mut payload = Message::new();
-        payload.embed(|embed| embed.title(&notification.title).description(&notification.message));
+        let embed =
+            CreateEmbed::new().title(&notification.title).description(&notification.message);
 
-        let res = self
-            .client
-            .post(&self.url)
-            .json(&payload)
-            .send()
+        let execute = ExecuteWebhook::new().embed(embed);
+
+        let webhook = Webhook::from_url(&self.http, &self.url)
+            .await
+            .map_err(|e| NotificationError::InvalidConfig(e.to_string()))?;
+
+        webhook
+            .execute(&self.http, true, execute)
             .await
             .map_err(|e| NotificationError::Transport(e.to_string()))?;
 
-        let status = res.status();
-
-        if status.is_success() {
-            return Ok(());
-        } else {
-            let msg = format!("({}) {}", status, res.text().await.unwrap());
-
-            let err = match status {
-                StatusCode::UNAUTHORIZED | StatusCode::FORBIDDEN => NotificationError::Auth(msg),
-                StatusCode::BAD_REQUEST => NotificationError::InvalidConfig(msg),
-                _ => NotificationError::Transport(msg),
-            };
-            Err(err)
-        }
+        Ok(())
     }
 
     fn name(&self) -> &'static str {
