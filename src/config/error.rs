0da785b85a888f8f468d32389e8ad7c7471e01c0
2kybe3 <kybe@kybe.xyz>
2026-01-21 09:48:26 +0100

    change volumes

--- src/config/error.rs
+++ src/config/error.rs
@@ -2,21 +2,21 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum ConfigError {
-	#[error("failed to get current directory")]
+	#[error("failed to get current directory: {0}")]
 	CurrentDir(#[source] std::io::Error),
 
-	#[error("failed to read config.toml")]
+	#[error("failed to read config.toml: {0}")]
 	ReadFile(#[source] std::io::Error),
 
-	#[error("failed to write default config.toml")]
+	#[error("failed to write default config.toml: {0}")]
 	WriteFile(#[source] std::io::Error),
 
-	#[error("invalid config.toml")]
+	#[error("invalid config.toml: {0}")]
 	Parse(#[from] toml::de::Error),
 
-	#[error("failed to download default config")]
+	#[error("failed to download default config: {0}")]
 	Download(#[from] reqwest::Error),
 
-	#[error("failed to serialize config")]
+	#[error("failed to serialize config: {0}")]
 	Serialize(#[from] toml::ser::Error),
 }
