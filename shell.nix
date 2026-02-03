62fb5df0b829cac531dd93628734e01c9a551429
2kybe3 <kybe@kybe.xyz>
2026-02-03 20:23:13 +0100

    improve nix shell and update dependencies

--- shell.nix
+++ shell.nix
@@ -1,10 +1,27 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-pkgs.mkShell {
-  buildInputs = [
-    pkgs.openssl
-    pkgs.gcc
-    pkgs.pkg-config
-    pkgs.sqlx-cli
-  ];
-}
+let
+  rust-overlay = fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz";
+  pkgs = import <nixpkgs> {
+    overlays = [
+      (import rust-overlay)
+      (_: prev: {
+        rust-toolchain = prev.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      })
+    ];
+  };
+in
+pkgs.callPackage (
+  {
+    mkShell,
+    rust-toolchain,
+  }:
+  mkShell {
+    strictDeps = true;
+    nativeBuildInputs = [
+      rust-toolchain
+      pkgs.openssl
+      pkgs.gcc
+      pkgs.pkg-config
+      pkgs.sqlx-cli
+    ];
+  }
+) { }
