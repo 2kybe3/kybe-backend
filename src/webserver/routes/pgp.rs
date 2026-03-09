fdb20fda35c071e4841288ddea1ac99089355234
2kybe3 <kybe@kybe.xyz>
2026-03-09 20:15:36 +0100

    reorder

--- src/webserver/routes/pgp.rs
+++ src/webserver/routes/pgp.rs
@@ -30,20 +30,10 @@ pub async fn pgp(
 			.title("Hello Stranger, and maybe PGP user :-)\n\n")
 			.into(),
 		CodeBlockBuilder::new(vec![
-			TextBlobBuilder::new(include_str!("../../../static/pgp.txt").trim()).into(),
-		])
-		.title("kybe <kybe@kybe.xyz> | 4B2067C3BD6D410F13E536A343CE43938A3C7A8F")
-		.into(),
-		CodeBlockBuilder::new(vec![
 			TextBlobBuilder::new("$ ").copyable(false).into(),
 			TextBlobBuilder::new("curl https://kybe.xyz/pgp.txt | gpg --import").into(),
 		])
-		.into(),
-		CodeBlockBuilder::new(vec![
-			TextBlobBuilder::new("$ ").copyable(false).into(),
-			TextBlobBuilder::new("ssh ssh.kybe.xyz pgp | gpg --import").into(),
-		])
-		.title("IPv6 required")
+		.title("Curl")
 		.into(),
 		CodeBlockBuilder::new(vec![
 			TextBlobBuilder::new("$ ").copyable(false).into(),
@@ -51,6 +41,17 @@ pub async fn pgp(
 				.into(),
 		])
 		.title("RFC7929")
+		.into(),
+		CodeBlockBuilder::new(vec![
+			TextBlobBuilder::new("$ ").copyable(false).into(),
+			TextBlobBuilder::new("ssh ssh.kybe.xyz pgp | gpg --import").into(),
+		])
+		.title("SSH (IPv6 required)")
+		.into(),
+		CodeBlockBuilder::new(vec![
+			TextBlobBuilder::new(include_str!("../../../static/pgp.txt").trim()).into(),
+		])
+		.title("kybe <kybe@kybe.xyz> | 4B2067C3BD6D410F13E536A343CE43938A3C7A8F")
 		.into(),
 	];
 	page.append(&mut common::footer::footer(trace.lock().await.trace_id));
