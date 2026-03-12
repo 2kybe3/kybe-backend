a97935a2b4ec453becd55a9bec0352a6efed7b8e
2kybe3 <kybe@kybe.xyz>
2026-03-12 22:49:05 +0100

    make flake simpler

--- flake.nix
+++ flake.nix
@@ -1,41 +1,30 @@
 {
-  description = "Rust dev shell";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs =
-    { nixpkgs, rust-overlay, ... }:
+    { nixpkgs, ... }:
     let
       system = "x86_64-linux";
 
       pkgs = import nixpkgs {
         inherit system;
-        overlays = [
-          rust-overlay.overlays.default
-          (_: prev: {
-            rust-toolchain = prev.rust-bin.fromRustupToolchainFile ./toolchain.toml;
-          })
-        ];
       };
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        strictDeps = true;
+         buildInputs = with pkgs; [
+          cargo
+          rustc
+          clippy
+          rustfmt
+          rust-analyzer
 
-        nativeBuildInputs = [
-          pkgs.rust-toolchain
-          pkgs.pkg-config
-          pkgs.openssl
-          pkgs.sqlx-cli
-          pkgs.gcc
+          sqlx-cli
         ];
 
-        shellHook = ''
-          export PKG_CONFIG_PATH=${pkgs.openssl.dev}/lib/pkgconfig
-        '';
+        env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
       };
     };
 }
