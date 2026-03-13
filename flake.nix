6590d2ae20c99e7e9480030d827549d258d81d44
2kybe3 <kybe@kybe.xyz>
2026-03-13 18:18:33 +0100

    tweak flake.nix

--- flake.nix
+++ flake.nix
@@ -1,36 +1,38 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            rustc
+            clippy
+            rustfmt
+            openssl
+            pkg-config
+            rust-analyzer
 
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-         buildInputs = with pkgs; [
-          cargo
-          rustc
-          clippy
-          rustfmt
-          openssl
-          pkg-config
-          rust-analyzer
+            sqlx-cli
+          ];
 
-          sqlx-cli
-        ];
+          env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 
-        env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-
-        shellHook = ''
-          export PKG_CONFIG_PATH=${pkgs.openssl.dev}/lib/pkgconfig
-        '';
-      };
-    };
+          shellHook = ''
+            export PKG_CONFIG_PATH=${pkgs.openssl.dev}/lib/pkgconfig
+          '';
+        };
+      }
+    );
 }
